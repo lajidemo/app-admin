c00d1b7b3c0f6b9d9e12f3467780743bd6223ddf
lajidemo <289455964@qq.com>
2020-05-13 21:44:58 +0800

    去除无用代码，添加测试代码

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button } from 'antd';
-import './App.css';
+// import { Button } from 'antd';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Home type="primary"></Home>
     </div>
   );
 }
